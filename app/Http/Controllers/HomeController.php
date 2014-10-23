08eff92de7b4e7a7c3cbb9911fadb28f416eaf83
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-23 11:31:06 +0200

    Added themed() helper function that translates view path to view related to current theme (default for now)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,24 +4,13 @@ use Illuminate\Routing\Controller;
 
 class HomeController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| Controller methods are called when a request enters the application
-	| with their assigned URI. The URI a method responds to may be set
-	| via simple annotations. Here is an example to get you started!
-	|
-	*/
-
 	/**
 	 * @Get("/")
 	 */
 	public function index()
 	{
 		
-
+		return themed('blog.index');
 	}
 
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+* returns singleton instance of module
+* @return \App\Module
+*/
+
 function module($name) {
 
 	$class = '\Kkstudio\\' . $name . '\\' . $name;
 
-	return $class;
 	return \App\Module::getInstance($class);
+
+}
+
+/**
+* translates view name to theme's view path
+* @return String
+*/
+
+function themed($name) {
+
+	$theme = 'default';
+
+	return $theme . '.' . $name;
 
 }
